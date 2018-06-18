3f0c2e6e41e0ca2ad091754dddd1f4e0ec09b62c
Devin <devincorrigall@gmail.com>
2018-06-17 21:34:14 -0700

    Set up inputs and store values

--- App.js
+++ App.js
@@ -1,13 +1,74 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View, Button, Alert } from 'react-native';
 
 export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      principal: '',
+      amortization: '',
+      interestRate: ''
+    };
+  }
+
+  handleInputChange = (event) => {
+    let text = event.nativeEvent.text;
+    if (/^\d*$/.test(text)) {
+      let name;
+      switch (event.target) {
+        case 6:
+          name = 'principal';
+          break;
+        case 9:
+          name = 'amortization';
+          break;
+        case 13:
+          name = 'interestRate';
+          break;
+      }
+
+      this.setState({
+        [name]: text
+      });
+    }
+  }
+
   render() {
     return (
       <View style={styles.container}>
-        <Text>Open up App.js to start working on your app!</Text>
-        <Text>Shake your phone to open the developer menu.</Text>
-        <Text>Test</Text>
+        <Text>Mortgage Payment Calculator</Text>
+        <Text>Principal (purchase price - down payment):</Text>
+          <TextInput 
+            style={styles.textInput}
+            keyboardType='numeric'
+            onChange={this.handleInputChange}
+            value={this.state.principal}
+            maxLength={10}
+          />
+
+          <Text>Amortization:</Text>
+          <TextInput 
+            style={styles.textInput}
+            keyboardType='numeric'
+            onChange={this.handleInputChange}
+            value={this.state.amortization}
+            maxLength={10}
+          />
+
+          <Text>Interest Rate:</Text>
+          <TextInput 
+            style={styles.textInput}
+            keyboardType='numeric'
+            onChange={this.handleInputChange}
+            value={this.state.interestRate}
+            maxLength={10}
+          />
+        <Button
+          onPress={() => {
+            Alert.alert(`Principal: ${this.state.principal} Amortization: ${this.state.amortization} InterestRate: ${this.state.interestRate}` );
+          }}
+          title="Values"
+        />
       </View>
     );
   }
